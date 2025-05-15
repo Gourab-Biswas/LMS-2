6ae032ca9abe587bd6a03028e88291c882f66efc
Gourab-Biswas <gourab1331@outlook.com>
2025-05-15 22:56:14 +0530

    try to all reverse

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -3,21 +3,23 @@ import Course from "../models/Course.js";
 // Get All Courses
 export const getAllCourse = async (req, res) => {
   try {
-    const courses = await Course.find({ isPublished: true }).select(['-courseContent', '-enrolledStudents']).populate({ path: 'educator' })
+    const courses = await Course.find({ isPublished: true })
+      .select(['-courseContent', '-enrolledStudents'])
+      .populate({ path: 'educator' });
 
-    res.json({ success: true, courses })
+    res.json({ success: true, courses });
   } catch (error) {
-    res.json({ success: false, message: error.message })
+    res.json({ success: false, message: error.message });
   }
 };
 
 
 // get course by id
 export const getCourseId = async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   try {
-    const courseData = await Course.findById(id).populate({ path: 'educator' })
+    const courseData = await Course.findById(id).populate({ path: 'educator' });
 
     // Remove lectureUrl if isPreviewFree is false
     courseData.courseContent.forEach(chapter => {
@@ -25,12 +27,12 @@ export const getCourseId = async (req, res) => {
         if (!lecture.isPreviewFree) {
           lecture.lectureUrl = "";
         }
-      })
-    })
+      });
+    });
 
     res.json({ success : true, courseData});
   } catch (error) {
-    res.json({ success: false, message: error.message })
+    res.json({ success: false, message: error.message });
     
   }
-}
+};
